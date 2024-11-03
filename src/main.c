ef39387c4cbe457a779c9311ba110158626aa15e
Guy Avraham <guy.avraham@rbbn.com>
2024-11-04 00:38:13 +0200

    remove function to other file

--- src/main.c
+++ src/main.c
@@ -1,35 +1,5 @@
 #include "utils/include/utils.h"
-#include "listener/include/listener.h"
-#include "dataBaseManager/include/dataBaseManager.h"
 #include "systemManager/include/systemManager.h"
-
-int componentTestEnterRunLoopAndCaptureSinglePacket()
-{
-    const char funcName [] = "componentTestEnterRunLoopAndCaptureSinglePacket - ";
-    int ret = systemMgrInit();
-    if (0 != ret)
-    {
-        return -1;
-    }
-
-    ret = listenerPrepareToEnterRunLoop();
-    if (0 != ret)
-    {
-        printf("%s the listenerPrepareToEnterRunLoop function was not successfull, aborting\n", funcName);
-        return -1;
-    }
-
-    listenerRunLoop();
-    ret = listenerCleanupAfterRunLoop();
-    if (0 != ret)
-    {
-        printf("%s the listenerCleanupAfterRunLoop function was not successfull, aborting\n", funcName);
-        return -1;
-    }
-
-    systemMgrTerminate();
-    return ret;
-}
 
 int main(int argc, char** argv)
 {
@@ -37,9 +7,9 @@ int main(int argc, char** argv)
     printf("%s ===START===\n", funcName);
     utilsGetCmdLineArgs(argc, argv);
 
-    int ret = componentTestEnterRunLoopAndCaptureSinglePacket();
-    printf("%s the componentTestEnterRunLoopAndCaptureSinglePacket function returned:%d\n", funcName, ret);
+    int ret = systemMgrRunProgram();
+    printf("%s the systemMgrRunProgram function returned:%d\n", funcName, ret);
 
     printf("%s ===END===\n", funcName);
-    return 0;
+    return ret;
 }
